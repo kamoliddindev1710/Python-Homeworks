2d4904f2ef0b317b710cbe880ef3870d23b1ebbd
kamoliddindev1710 <kamoliddinfazliddinov17@gmail.com>
2025-04-28 21:35:19 +0500

    Create task.py

---
+++ lesson-1/homework/task.py
@@ -0,0 +1,29 @@
+# import math 
+# task 1 
+# height=int(input('Enter the height of square '))
+# width=int(input('Enter the width of square '))
+
+# area=height*width
+# perimetr=2*(height+width)
+# print(f'Area od square is {area} and perimetr is {perimetr}')
+
+# task 2
+# diametr=float(input('Enter the diametr of Circle '))
+# p=3.14
+# Lenght=diametr*p
+# print(f'Length of circle is {Lenght}')
+
+#task 3
+# a=int(input('Number for a '))
+# b=int(input('Number for b '))
+# c=(a+b)/2
+# print(c)
+
+#task 4
+# a=int(input('Number for a '))
+# b=int(input('Number for b '))
+# total=a+b
+# product=a*b
+# square_1=pow(a,2)
+# square_2=pow(b,2)
+# print(f'Sum is {total},Product is {product},First number square is {square_1},Second number square is {square_2}')
